c3e9e23f1cb02f9322f5f5674050dcf773254320
Fernando <fernandodof@gmail.com>
2015-03-04 17:55:19 -0300

    preventing access to subscription page when logged in

--- pages/subscribe.php
+++ pages/subscribe.php
@@ -1,5 +1,15 @@
 <?php
 require_once './smartyHeader.php';
+
+if (!session_id()) {
+    session_start();
+}
+
+if(isset($_SESSION['id'])){
+    header('Location: ../pages/index');
+}
+
+
 include_once '../pages/header.php';
 
 $smarty->display('../templates/subscribe.tpl');
