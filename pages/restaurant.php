c19bf560baf20fd98babefbc3b32f8259c477ba5
unknown <fernandodof@gmail.com>
2015-03-04 06:57:42 -0300

    go

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -13,4 +13,91 @@ require_once $path . 'src/app/util/UserTypes.php';
 session_start();
 
 $slashCount = substr_count(filter_input(INPUT_SERVER, 'REQUEST_URI'), '/');
-echo $slashCount;
+
+if ($slashCount < 3) {
+    header("Location: ../error");
+}
+
+list(,,, $res) = explode('/', filter_input(INPUT_SERVER, 'REQUEST_URI'));
+
+$dao = new Dao();
+if (isset($_SESSION['idRestauranteDoPedidoAtual'])) {
+    $restaurantePedido = $dao->findByKey('Restaurante', $_SESSION['idRestauranteDoPedidoAtual']);
+    $smarty->assign('restaurantePedido', $restaurantePedido);
+}
+
+$restaurante = $dao->findByKey('Restaurante', $res);
+
+if ($restaurante == null) {
+    header("Location: ../error");
+}
+
+include_once '../pages/header.php';
+
+$produtos = $restaurante->getProdutos();
+
+$produtosComida = array();
+$produtosBebida = array();
+
+foreach ($produtos as $p) {
+    if ($p->getCategoria()->getNome() == 'Comida') {
+        $produtosComida[] = $p;
+    } else if ($p->getCategoria()->getNome() == 'Bebida') {
+        $produtosBebida[] = $p;
+    }
+}
+
+$avgRating;
+$sum = 0;
+$counter = 0;
+foreach ($restaurante->getAvaliacoes() as $av) {
+    $sum += $av->getNota();
+    $counter++;
+}
+
+if ($counter > 0) {
+    $avg = $sum / $counter;
+} else {
+    $avg = 0;
+}
+
+$avgRating = $avg;
+
+
+foreach ($produtosComida as $p) {
+    $avgRatingP;
+    $sumP = 0;
+    $counter = 0;
+
+    foreach ($p->getAvaliacoes() as $av) {
+        $sumP += $av->getNota();
+        $counter++;
+    }
+
+    if ($counter > 0) {
+        $avgP = $sumP / $counter;
+    } else {
+        $avgP = 0;
+    }
+
+    $avgRatingP[] = $avgP;
+}
+
+if (isset($avgRatingP)) {
+    $smarty->assign('avgRatingP', $avgRatingP);
+}
+
+if (isset($_SESSION['id']) && $_SESSION['tipo'] === UserTypes::CLIENTE) {
+    $params1['id_cliente'] = $_SESSION['id'];
+    $idsProdutosComprados = $dao->getListResultOfNativeQueryWithParameters(Queries::GET_IDS_PRODUTOS_CLIENTE_COMPROOU, $params1);
+    $smarty->assign('idsProdutosComprados', $idsProdutosComprados);
+}
+
+$smarty->assign('avgRating', $avgRating);
+$smarty->assign('produtosComida', $produtosComida);
+$smarty->assign('produtosBebida', $produtosBebida);
+$smarty->assign('restaurante', $restaurante);
+
+$smarty->display($path . 'templates/restaurant.tpl');
+
+include_once $path . 'pages/footer.php';
